633139692cb810080027649ee73c537a026686af
doraly <doraly9110@hotmail.com>
2022-05-03 11:35:06 -0500

    error1

--- systempolls/settings.py
+++ systempolls/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
